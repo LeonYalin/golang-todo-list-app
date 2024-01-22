4c0b206f32cc63ddf26c3410a65220731e254bb1
Leon Yalin <leon.yalin@irisity.com>
2024-01-22 12:46:46 +0200

    add edit_todo_dialog, edit_todo_options

--- src/todo_manager.go
+++ src/todo_manager.go
@@ -12,17 +12,28 @@ import (
 )
 
 type TodoManager struct {
-	io              IOManageable
-	todos           []*models.Todo
-	welcome_options []types.TodoOption
+	io                IOManageable
+	todos             []*models.Todo
+	selectedTodoId    string
+	welcome_options   []types.TodoOption
+	edit_todo_options []types.TodoOption
 }
 
 func (tm *TodoManager) SetWelcomeOptions(options []types.TodoOption) {
 	tm.welcome_options = options
 }
 
+func (tm *TodoManager) SetEditTodoOptions(options []types.TodoOption) {
+	tm.edit_todo_options = options
+}
+
 func (tm *TodoManager) AddTodo(description string) {
 	tm.todos = append(tm.todos, models.NewTodo(description))
+}
+
+func (tm *TodoManager) SelectedTodo() *models.Todo {
+	i, _ := strconv.Atoi(tm.selectedTodoId)
+	return tm.todos[i]
 }
 
 func (tm *TodoManager) Print() string {
@@ -43,8 +54,8 @@ func (tm *TodoManager) greetingDialog() {
 }
 
 func (tm *TodoManager) mainMenuDialog() {
-	for i, option := range tm.welcome_options {
-		tm.io.Write(util.OptionText(strconv.Itoa(i+1), option.Text) + "\n")
+	for _, option := range tm.welcome_options {
+		tm.io.Write(util.OptionText(option.Key, option.Text) + "\n")
 	}
 	answer := tm.io.Question(util.ENTER_AN_OPTION_TEXT, func(ans string) bool {
 		return slices.ContainsFunc(tm.welcome_options, func(item types.TodoOption) bool {
@@ -77,7 +88,32 @@ func (tm *TodoManager) viewAllTodosDialog() {
 	if answer == types.GO_BACK {
 		tm.mainMenuDialog()
 	} else {
-		// TODO
+		i, _ := strconv.Atoi(answer)
+		tm.selectedTodoId = tm.todos[i-1].ID
+		tm.editTodoDialog()
+	}
+}
+
+func (tm *TodoManager) editTodoDialog() {
+	tm.io.Write(fmt.Sprintf("\n%s %s\n\n", util.YellowText("Selected:"), util.TodoText(-1, tm.SelectedTodo())))
+
+	for _, option := range tm.edit_todo_options {
+		tm.io.Write(util.OptionText(option.Key, option.Text) + "\n")
+	}
+	tm.io.Write(fmt.Sprintf("%s\n", util.OptionText(types.GO_BACK, "Go back")))
+
+	answer := tm.io.Question(util.ENTER_AN_OPTION_TEXT, func(ans string) bool {
+		ansInt, err := strconv.Atoi(ans)
+		return ans != "" && err == nil && ansInt >= 0 && ansInt <= len(tm.todos)
+	})
+	if answer == types.GO_BACK {
+		tm.selectedTodoId = ""
+		tm.viewAllTodosDialog()
+	} else {
+		chosenOptionIndex := slices.IndexFunc(tm.edit_todo_options, func(to types.TodoOption) bool {
+			return to.Key == answer
+		})
+		tm.edit_todo_options[chosenOptionIndex].Handler()
 	}
 }
 
@@ -91,16 +127,35 @@ func (tm *TodoManager) addTodoDialog() {
 	tm.mainMenuDialog()
 }
 
+func (tm *TodoManager) editTodoDescriptionDialog() {
+	fmt.Printf("editTodoDescriptionDialog")
+}
+
+func (tm *TodoManager) toggleTodoCompletedDialog() {
+	fmt.Printf("toggleTodoCompletedDialog")
+}
+
+func (tm *TodoManager) deleteTodoDialog() {
+	fmt.Printf("deleteTodoDialog")
+}
+
 func (tm *TodoManager) exitDialog() {
 	tm.io.Write(fmt.Sprintf("%s\n\n", util.YellowText("Exiting.. Bye!")))
 }
 
 func NewTodoManager(io IOManageable) *TodoManager {
-	tm := &TodoManager{io: io, todos: []*models.Todo{}, welcome_options: []types.TodoOption{}}
+	tm := &TodoManager{io: io, todos: []*models.Todo{}, selectedTodoId: "", welcome_options: []types.TodoOption{}, edit_todo_options: []types.TodoOption{}}
+
 	tm.SetWelcomeOptions([]types.TodoOption{
 		{Key: "1", Text: "View all todos", Handler: func(args ...any) { tm.viewAllTodosDialog() }},
 		{Key: "2", Text: "Add a todo", Handler: func(args ...any) { tm.addTodoDialog() }},
 		{Key: "3", Text: "Exit", Handler: func(args ...any) { tm.exitDialog() }},
+	})
+
+	tm.SetEditTodoOptions([]types.TodoOption{
+		{Key: "1", Text: "Edit description", Handler: func(args ...any) { tm.editTodoDescriptionDialog() }},
+		{Key: "2", Text: "Toggle completed", Handler: func(args ...any) { tm.toggleTodoCompletedDialog() }},
+		{Key: "3", Text: "Delete todo", Handler: func(args ...any) { tm.deleteTodoDialog() }},
 	})
 	return tm
 }
